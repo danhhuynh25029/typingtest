fbd55a57e9a8ff9ec66b29efa940fae540f7b037
danhhuynh25029 <danhhuynh25029@gmail.com>
2021-07-30 20:18:45 +0700

    add code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-function myFunction() {
-    document.getElementById("myText").value = "Johnny Bravo";
+function myFunction(ele) {
+   if(event.key === "Enter"){
+        var text = document.getElementById("text").innerHTML;
+        if(text == ele.value){
+           console.log(1)
+           document.getElementById("result").innerHTML = "correct";
+           document.getElementById("result").style.color = "green";
+         //   span.classList.add('red');
+        }else{
+           document.getElementById("result").innerHTML = "incorrect";
+           document.getElementById("result").style.color = "red";
+           console.log(0);
+        }
+   }
 }
