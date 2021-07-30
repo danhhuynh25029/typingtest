f4771c9e12ef0a72378e0728eb5594212bb1f1aa
danhhuynh25029 <danhhuynh25029@gmail.com>
2021-07-30 23:21:37 +0700

    change code main.js

--- js/main.js
+++ js/main.js
@@ -3,22 +3,23 @@ function myFunction(ele) {
         var text = document.getElementById("text").innerHTML;
         if(text == ele.value){
            
-           index += 1
-           changeData(index)
+           index = Math.floor(Math.random() * 3);
+           changeText(index)
+           console.log(index);
            document.getElementById("result").innerHTML = "correct";
            document.getElementById("result").style.color = "green";
          //   span.classList.add('red');
         }else{
            document.getElementById("result").innerHTML = "incorrect";
            document.getElementById("result").style.color = "red";
-           console.log(0);
+         //   console.log(0);
         }
    }
 }
-var index = 0;
+var index = Math.floor(Math.random() * 3);
 var text = ["With you","hello world","My name is Lisa"]
-function changeData(index){
+function changeText(index){
    document.getElementById("text").innerHTML = text[index]
    document.getElementById("myText").value = "";
 }
-changeData(index)
+changeText(index)
